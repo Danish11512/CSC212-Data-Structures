0e280b7f09fed57fa0e276cceb8c6a132594c2b1
Danish Faruqi <31814461+Danish11512@users.noreply.github.com>
2018-11-27 15:36:27 -0500

    Add files via upload

---
+++ Project 5/cmake-build-debug/CMakeFiles/Project_5.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Project_5.dir/maze.cpp.obj"
+  "CMakeFiles/Project_5.dir/recursion.cpp.obj"
+  "Project_5.pdb"
+  "Project_5.exe"
+  "Project_5.exe.manifest"
+  "libProject_5.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Project_5.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
